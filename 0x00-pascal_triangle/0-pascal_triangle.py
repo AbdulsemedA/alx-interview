d5e02324e52a2d868066c934309b4c05d2b09641
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-12-20 16:47:02 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -16,8 +16,12 @@ def pascal_triangle(n):
         list1.append([])
         #append first 1
         list1[i].append(1)
+        #loop for insides
         for j in range(1, i):
+            #add insides
             list1[i].append(list1[i - 1][j - 1] + list1[i - 1][j])
+        #check not first
         if(i != 0):
+            #append last 1
             list1[i].append(1)
     return list1
