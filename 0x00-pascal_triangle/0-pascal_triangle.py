b09213380c76fc46636c87bbb01e246b497d7915
Abdulsemed Abdelila Andya <88716122+AbdulsemedA@users.noreply.github.com>
2022-12-20 11:46:13 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,15 +3,15 @@
 0-pascal_triangle
 """
 def pascal_triangle(n):
-  """
-  find the pascal triange from a given integer size
-  """
-  list1 = []
-  for i in range(n):
-    list1.append([])
-    list1[i].append(1)  
-    for j in range(1, i):  
-      list1[i].append(list1[i - 1][j - 1] + list1[i - 1][j])  
-    if(i != 0):  
-      list1[i].append(1)
-  return list1 if n > 0 else []
+    """
+    find the pascal triange from a given integer size
+    """
+    list1 = []
+    for i in range(n):
+      list1.append([])
+      list1[i].append(1)  
+      for j in range(1, i):  
+        list1[i].append(list1[i - 1][j - 1] + list1[i - 1][j])  
+      if(i != 0):  
+        list1[i].append(1)
+    return list1 if n > 0 else []
